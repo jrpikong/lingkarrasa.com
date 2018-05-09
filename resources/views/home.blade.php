462d4c2e73ae96ce2d6f0d60e48bcbecaed9d235
jrpikong <jr.pikong@gmail.com>
2018-05-09 17:29:51 -0400

    hidden news and sidebar for mobile

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -35,7 +35,7 @@
                     </blockquote>
                 </div>
             </div>
-            <div class="card">
+            <div class="card d-none d-lg-block">
                 <div class="card flex-md-row box-shadow">
                     <div class="card-body flex-column align-items-start">
                         <h5 class="mb-0"><a href="#" class="text-dark">Featured post</a></h5>
@@ -220,11 +220,9 @@
                     </div>
                 </div>
 
-
-
                 <button type="button" class="btn btn-primary btn-lg btn-block mt-5 mb-5">Load More</button>
             </div>
-            <div class="col-md-4">
+            <div class="col-md-4 d-none d-lg-block">
                 <div class="card" style="position: sticky">
                     <img class="card-img-top" data-src="holder.js/100px180/?text=Ads" alt="Image cap [100%x180]" style="height: 172.7px; width: 100%; display: block;" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22286%22%20height%3D%22180%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20286%20180%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_163404a6316%20text%20%7B%20fill%3Argba(255%2C255%2C255%2C.75)%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_163404a6316%22%3E%3Crect%20width%3D%22286%22%20height%3D%22180%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2294%22%20y%3D%2296.9%22%3EImage%20cap%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" data-holder-rendered="true">
                     <div class="card-body">
